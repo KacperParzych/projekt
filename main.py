85659be81eaa9e9bc365accaa47774f07f807921
KacperParzych <157218473+KacperParzych@users.noreply.github.com>
2024-03-03 12:52:36 +0100

    Zmiana2

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ print(type(b))
 ZmiennaTekstowa = 'Wizualizacja danych'
 print(ZmiennaTekstowa)
 print(type(ZmiennaTekstowa))
+
+#Dzialania matemtyczne
+
+Liczba1 = 6
+Liczba2 = 3
+
+Wynik=Liczba1+Liczba2
+print(Wynik)
+Wynik2=Liczba1-Liczba2
+print(Wynik2)
+
+Liczba3 = 4
+Wynik3=Liczba2//Liczba1
+print(Wynik3)
+
+Wynik4=Liczba1//Liczba2
+print(Wynik4)
